e6f4bad7d03f760a0abff41c38e4502f56ba2840
Billibear <93044176+attwice@users.noreply.github.com>
2022-04-05 21:19:23 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Fragment } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/navbar.css';
+import './styles/main.css';
+import Mint from './components/mint';
+
+function App() {
+  return (
+    <Fragment>
+      <Mint />
+    </Fragment>
+  );
+}
+
+export default App;
